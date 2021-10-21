1de1c1ddde3b3257c091ab434fb59376d88c561a
Shi <stoneshishang@gmail.com>
2021-10-21 13:30:57 -0400

    add Automatic Mapping of Gateway Routes

---
+++ ShangApiGateway/src/main/java/com/example/shangapigateway/ShangApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.shangapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShangApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShangApiGatewayApplication.class, args);
+    }
+
+}
