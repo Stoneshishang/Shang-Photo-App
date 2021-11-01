4fdac34331106ad4195abd415093fffca3a818dc
Shi <stoneshishang@gmail.com>
2021-11-01 15:58:37 -0400

    add spring security including encryped user password and allow only my computer to access the API

--- ShangPhotoAppApiUsers/src/main/java/com/example/shangphotoappapiusers/ShangPhotoAppApiUsersApplication.java
+++ ShangPhotoAppApiUsers/src/main/java/com/example/shangphotoappapiusers/ShangPhotoAppApiUsersApplication.java
@@ -3,6 +3,8 @@ package com.example.shangphotoappapiusers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,6 +13,11 @@ public class ShangPhotoAppApiUsersApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(ShangPhotoAppApiUsersApplication.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
--- ShangPhotoAppApiUsers/src/main/java/com/example/shangphotoappapiusers/serivce/UsersServiceImpl.java
+++ ShangPhotoAppApiUsers/src/main/java/com/example/shangphotoappapiusers/serivce/UsersServiceImpl.java
@@ -7,28 +7,32 @@ import com.example.shangphotoappapiusers.shared.UserDto;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsersServiceImpl implements UsersService {
 
     UsersRepository usersRepository;
+    BCryptPasswordEncoder bCryptPasswordEncoder;
 
     //Constructor based dependency injection
     @Autowired
-    public UsersServiceImpl(UsersRepository usersRepository){
+    public UsersServiceImpl(UsersRepository usersRepository, BCryptPasswordEncoder bCryptPasswordEncoder){
         this.usersRepository = usersRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @Override
     public UserDto createUser(UserDto userDetails) {
 
         userDetails.setUserId(UUID.randomUUID().toString());
+        userDetails.setEncryptedPassword(bCryptPasswordEncoder.encode(userDetails.getPassword()));
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
         UserEntity userEntity= modelMapper.map(userDetails, UserEntity.class);
-        userEntity.setEncryptedPassword("test");
+
 
         usersRepository.save(userEntity);
 
